b678dca701e5aef774763a076564a1e5f64634d0
Geneyuu <eugene.escario@cvsu.edu.ph>
2025-03-11 21:56:22 +0800

    fixed minimal bugs on styling

--- app/(tabs)/home/in-place/components/StartWarmups/TimerControls.jsx
+++ app/(tabs)/home/in-place/components/StartWarmups/TimerControls.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import * as Progress from "react-native-progress";
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const TimerControls = ({
 	timer,
@@ -10,11 +15,17 @@ const TimerControls = ({
 	stopWarmup,
 	restartWarmup,
 	resetTimerAndVideo,
+	totalTimerDuration,
+	nextExercise,
 }) => {
-	return (
-		<View>
-			<Text style={styles.timerText}>{formatTime(timer)}s</Text>
+	const progress =
+		timer >= totalTimerDuration
+			? 0
+			: Math.min(1, 1 - (timer - 1) / totalTimerDuration);
 
+	return (
+		<View style={styles.container}>
+			{/*  Buttons */}
 			{!isResting && (
 				<>
 					<TouchableOpacity
@@ -23,10 +34,11 @@ const TimerControls = ({
 					>
 						<Ionicons
 							name="refresh-circle-outline"
-							size={37}
+							size={hp("3.7%")}
 							color="black"
 						/>
 					</TouchableOpacity>
+
 					<TouchableOpacity
 						style={[
 							styles.button,
@@ -42,14 +54,68 @@ const TimerControls = ({
 								: "Start Exercise"}
 						</Text>
 					</TouchableOpacity>
+
+					<TouchableOpacity
+						style={[styles.button, styles.restartButton]}
+						onPress={restartWarmup}
+					>
+						<Text style={styles.buttonText}>Restart Exercises</Text>
+					</TouchableOpacity>
 				</>
 			)}
-			<TouchableOpacity
-				style={[styles.button, styles.restartButton]}
-				onPress={restartWarmup}
-			>
-				<Text style={styles.buttonText}>Restart Exercises</Text>
-			</TouchableOpacity>
+			{/*  Progress */}
+			<View style={styles.progressWrapper}>
+				{isResting ? (
+					<>
+						<Progress.Circle
+							progress={progress}
+							size={wp("60%")}
+							color="green"
+							unfilledColor="#e0e0e0"
+							borderWidth={0}
+							thickness={hp("3%")}
+							animated
+							style={{ marginTop: 25 }}
+						/>
+						<View style={styles.restingTimerOverlay}>
+							<Text style={styles.timerTextInside}>
+								{formatTime(timer)}s
+							</Text>
+						</View>
+					</>
+				) : (
+					<>
+						<Progress.Bar
+							progress={progress}
+							width={wp("105%")}
+							color="green"
+							unfilledColor="#e0e0e0"
+							borderWidth={0}
+							height={hp("7%")}
+							borderRadius={hp("1%")}
+							animated
+						/>
+
+						<View style={styles.timerOverlay}>
+							<Text style={styles.timerTextInside}>
+								{formatTime(timer)}s
+							</Text>
+						</View>
+					</>
+				)}
+			</View>
+			{isResting && (
+				<TouchableOpacity
+					style={[
+						styles.button,
+						styles.restartButton,
+						{ marginTop: hp("2%") },
+					]}
+					onPress={restartWarmup}
+				>
+					<Text style={styles.buttonText}>Restart Exercises</Text>
+				</TouchableOpacity>
+			)}
 		</View>
 	);
 };
@@ -57,47 +123,86 @@ const TimerControls = ({
 const formatTime = (time) => {
 	const minutes = Math.floor(time / 60);
 	const seconds = time % 60;
-	return `${minutes < 10 ? "" + minutes : minutes}:${
+	return `${minutes < 10 ? minutes : minutes}:${
 		seconds < 10 ? "0" + seconds : seconds
 	}`;
 };
 
 const styles = StyleSheet.create({
-	timerText: {
-		fontSize: 60,
-		fontFamily: "Karla-Bold",
-		color: "#161616",
-		marginTop: 20,
-		textAlign: "center",
+	container: {
+		width: wp("100%"),
+		alignSelf: "center",
+		alignItems: "center",
 	},
+
+	progressWrapper: {
+		marginVertical: hp("1%"),
+		position: "relative",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	// For Progress.Bar overlay
+	timerOverlay: {
+		position: "absolute",
+		left: 0,
+		right: 0,
+		top: 0,
+		bottom: 0,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	// For Progress.Circle overlay
+	restingTimerOverlay: {
+		position: "absolute",
+		width: wp("70%"),
+		height: wp("60%"),
+		alignItems: "center",
+		justifyContent: "center",
+		top: 25,
+	},
+
+	timerTextInside: {
+		fontSize: hp("5%"),
+		fontFamily: "Karla-Bold",
+		color: "black",
+	},
+
 	resetIconContainer: {
 		position: "absolute",
-		left: 60,
-		top: 30,
+		left: wp("25%"),
+		top: hp("16.1%"),
 		alignItems: "center",
 		justifyContent: "center",
-		width: 50,
-		height: 50,
+		width: wp("10%"),
+		height: hp("5%"),
+		zIndex: 5,
 	},
+
 	button: {
-		paddingVertical: 15,
-		borderRadius: 10,
+		paddingVertical: hp("1.5%"),
+		width: wp("80%"),
+		borderRadius: hp("1%"),
 		justifyContent: "center",
 		alignItems: "center",
-		marginTop: 20,
+		marginTop: hp("1%"),
 	},
+
 	buttonText: {
 		color: "black",
-		fontSize: 18,
+		fontSize: hp("2%"),
 		fontFamily: "Karla-Bold",
 	},
+
 	startButton: { backgroundColor: "black" },
 	pauseButton: { backgroundColor: "#dc3545" },
-	restartButton: { borderWidth: 2, borderColor: "black", color: "black" },
+	restartButton: { borderWidth: 1, borderColor: "black" },
+
 	buttonExercise: {
 		color: "white",
 		fontFamily: "Karla-Bold",
-		fontSize: 18,
+		fontSize: hp("2%"),
 	},
 });
 
